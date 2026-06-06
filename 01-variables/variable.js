c7b47563422965ec6bb839668c865570ff711eec
suryacharan2 <ssuryachara2@gmail.com>
2026-06-06 14:41:33 +0530

    datatypes

--- 01-variables/variable.js
+++ 01-variables/variable.js
@@ -1,6 +1,7 @@
 let name = "surya charan";
 const age = 22;
 var isStudent = true;
+
 console.log("Name:", name);
 console.log("Age:", age);
 console.log("Is Student:", isStudent);
---
+++ datatypes/datatypes.js
@@ -0,0 +1,13 @@
+let name = "John Doe";//string
+let age = 30;//number
+let isStudent = true;//boolean
+let nullValue = null;//null
+let undefinedValue; //undefined
+let symbolValue = Symbol("$");//symbol
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Null Value:", nullValue);
+console.log("Undefined Value:", undefinedValue);
+console.log("Symbol Value:", symbolValue);
